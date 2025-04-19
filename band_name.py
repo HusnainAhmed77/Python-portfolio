02edded225160aa98e9a28252ce95a97776e6b57
HusnainAhmed77 <husnain77ahmed@gmail.com>
2025-04-20 02:25:47 +0500

    Add files via upload

---
+++ band_name.py
@@ -0,0 +1,13 @@
+#1. Create a greeting for your program.
+print( "Hi, this is my first program" + "I hope you like it")
+print( "I will be creating a band name for you so please answer the following questions\n")
+    
+#2. Ask the user for the city that they grew up in.
+city = input ("In which city did you grow up in ?: \n")
+
+#3. Ask the user for the name of a pet.
+pet_name = input ("Please enter your pet's name: \n")
+
+#4. Combine the name of their city and pet and show them their band name.
+band_name = city + pet_name
+print (band_name + "\n")
